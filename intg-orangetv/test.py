bd83ac3f68721b174d6a66805276e14cebe798f0
Albaintor <albaintor@github.com>
2025-04-18 12:06:52 +0200

    Added test script

---
+++ intg-orangetv/test.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+import sys
+from client import LiveboxTvUhdClient
+from config import DeviceInstance
+
+if sys.platform == 'win32':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+_LOOP = asyncio.new_event_loop()
+asyncio.set_event_loop(_LOOP)
+
+async def main():
+    _LOG.debug("Start connection")
+    client = LiveboxTvUhdClient(device_config=
+                      DeviceInstance(id="deviceid", name="LiveboxUHD", address="192.168.1.129",
+                                     port=8080, country="france", always_on=False))
+    await client.connect()
+    for i in range(5):
+        _LOG.debug("INFO %s %s", client.media_state, client.attributes)
+        await asyncio.sleep(10)
+
+if __name__ == "__main__":
+    _LOG = logging.getLogger(__name__)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    logging.basicConfig(handlers=[ch])
+    logging.getLogger("client").setLevel(logging.DEBUG)
+    logging.getLogger("lglib").setLevel(logging.DEBUG)
+    logging.getLogger("test_connection").setLevel(logging.DEBUG)
+    _LOOP.run_until_complete(main())
+    _LOOP.run_forever()
